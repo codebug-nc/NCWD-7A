337b38753b8d11b2af7cabea726e0b98e7202a27
camp user <camp@ma-15-camp-3.stlukesct.org>
2019-07-16 15:35:44 -0400

    ok

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
 $(document).ready(function() {
 // your javascript and jQuery code goes below
+$("#pink").click(function(){
+	console.log ("I've been clicked!");
+	$("#pink").css("border", "20px solid pink")
+
+
+
+
 
 })
+
+$("#blue").click(function(){
+	console.log ("I've been clicked!");
+	$("#container").css("background-color", "blue")
+
+})
+$("#yerrow").click(function(){
+	console.log ("I've been clicked!");
+	$("#container").css("background-color", "yellow")
+	
+
+	$("#container").fadeOut (1000)
+
+})
+
+
+
+
+
+
+})
+
