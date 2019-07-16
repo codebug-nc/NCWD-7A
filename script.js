5985a963841b1682c33543dff72bd8110b358cc3
camp user <camp@ma-15-camp-1.stlukesct.org>
2019-07-16 15:36:56 -0400

    I eddited a website

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
 $(document).ready(function() {
 // your javascript and jQuery code goes below
+$("#pink").click(function(){
 
+	console.log("I've been clicked!");
+	$("#pink").css("border", "20px solid pink");
+
+
+
+
+});
+$("#blue").click(function() {
+
+	console.log("Fealing blue")
+	$("#container").css("background-color", "blue")
+})
+$("#fade-away").click(function(){
+	console.log("fade away")
+	$("#container").fadeOut(2500);
+})
+$("#lottery").click(function(){
+	// console.log("you just won the lottery")
+	alert("you just won the lotter")
+})
+$("#confirm").click(function() {
+	confirm("Press a button!");
+})
+$("#puppy").click(function() {
+	console.log("puppy")
+	$("#container").css("background-image", "url('images/puppy.jpg')");
+})
 })
